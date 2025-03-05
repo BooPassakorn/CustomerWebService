b37f842e3d353705b27ae0fdf0ed2dddb14530c7
BooPassakorn <t.passakorn4999@gmail.com>
2025-03-05 08:11:03 +0700

    Initial commit

---
+++ src/test/java/th/co/cdg/Customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.cdg.Customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
