ee5318277918be8952eb7e2c8e030fc8cf22d4e9
BooPassakorn <t.passakorn4999@gmail.com>
2025-03-05 09:00:48 +0700

    Make getAllCustomerController

--- src/main/java/th/co/cdg/Customer/presentation/CustomerController.java
+++ src/main/java/th/co/cdg/Customer/presentation/CustomerController.java
@@ -1,9 +1,14 @@
 package th.co.cdg.Customer.presentation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import th.co.cdg.Customer.model.Customer;
 import th.co.cdg.Customer.repository.CustomerRepository;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/customer/api/")
@@ -13,6 +18,13 @@ public class CustomerController {
     CustomerRepository customerRepository;
 
     // ---- Service สำหรับข้อมูล Customer ทั้งหมด ---- //
+    @GetMapping(value = "get-all-customer")
+    public ResponseEntity<ArrayList<Customer>> getAllCustomerController() {
+        return ResponseEntity
+                .ok()
+                .body(customerRepository.queryAllCustomer());
+    }
+
     // ---- Service สำหรับข้อมูลตาม Id ---- //
     // ---- Service สำหรับเพิ่มข้อมูล Customer ---- //
     // ---- Service สำหรับอัพเดตข้อมูล Customer ---- //
--- src/main/java/th/co/cdg/Customer/repository/CustomerRepository.java
+++ src/main/java/th/co/cdg/Customer/repository/CustomerRepository.java
@@ -2,7 +2,12 @@ package th.co.cdg.Customer.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
+import th.co.cdg.Customer.model.Customer;
+
+import java.util.ArrayList;
 
 @Repository
 public class CustomerRepository {
@@ -10,4 +15,29 @@ public class CustomerRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Transactional(Transactional.TxType.SUPPORTS)
+    public ArrayList<Customer> queryAllCustomer() {
+
+        String sql = " SELECT *  " +
+                " FROM CUSTOMER ";
+
+        Query query = entityManager.createNativeQuery(sql);
+
+        ArrayList<Object[]> resultList = (ArrayList<Object[]>) query.getResultList();
+
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        resultList.forEach(result -> {
+            Customer customer = new Customer();
+            customer.setId(((Integer) result[0]).longValue());
+            customer.setName((String) result[1]);
+            customer.setSurname((String) result[2]);
+            customer.setAddress(result[3].toString());
+            customer.setAge(((Integer) result[4]).longValue());
+            customer.setTel((String) result[5]);
+            customers.add(customer);
+        });
+
+        return customers;
+    }
 }
